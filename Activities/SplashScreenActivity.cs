21e8d0cc3ae8035a6777149b4ffdbb0c65e257e1
Johan Paiz <johanedfb@gmail.com>
2024-02-23 00:39:57 -0600

    feat: Add Splash Screen and Home Activity

---
+++ Activities/SplashScreenActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.Content;
+using AndroidX.AppCompat.App;
+
+namespace SysCredit.Android.Activities;
+
+[Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/SplashScreen")]
+public class SplashScreenActivity : AppCompatActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        StartActivity(new Intent(this, typeof(HomeActivity)));
+        Finish();
+    }
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -1,13 +1,12 @@
+using Android.Content;
+
 namespace SysCredit.Android;
 
-[Activity(Label = "@string/app_name", MainLauncher = true)]
+[Activity(Label = "@string/app_name", MainLauncher = false)]
 public class MainActivity : Activity
 {
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
-
-        // Set our view from the "main" layout resource
-        SetContentView(Resource.Layout.activity_main);
     }
 }
