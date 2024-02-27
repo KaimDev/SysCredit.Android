0e6f79d128044304effab02375531e0d2ca44b44
Kenneth Lola <87386788+kenthLol@users.noreply.github.com>
2024-02-27 16:02:13 -0600

    feat: Add Slider Menu and Implement Fragment Views

---
+++ Fragments/HomeFragment.cs
@@ -0,0 +1,14 @@
+﻿using Android;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using AndroidX.DrawerLayout.Widget;
+
+namespace SysCredit.Android.Fragments;
+
+public class HomeFragment : Fragment
+{
+    public override View OnCreateView(LayoutInflater? inflater, ViewGroup? container, Bundle? savedInstanceState)
+    {
+        return inflater!.Inflate(Resource.Layout.fragment_home, container, false)!;
+    }
+}
